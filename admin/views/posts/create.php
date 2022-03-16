0c73a0251f26820c69a04f2635b1edcfc2c9b932
Shohanur Rahman Sumon <srsumon.dev@gmail.com>
2022-03-17 04:11:31 +0600

    user profile added to admin template

--- admin/views/posts/create.php
+++ admin/views/posts/create.php
@@ -1,3 +1,18 @@
+<?php
+include_once '../../../vendor/autoload.php';
+
+use Admin\Post\Post;
+
+session_start();
+
+if(!isset($_SESSION['user']) || empty($_SESSION['user'])) {
+  header("Location: ../users/login.php");
+  exit();
+}
+
+$post = new Post();
+
+?>
 <!DOCTYPE html>
 <html dir="ltr" lang="en">
 
@@ -243,14 +258,14 @@
                                 <img src="../../assets/images/users/1.jpg" alt="user" class="rounded-circle" width="31" />
                             </a>
                             <ul class="dropdown-menu dropdown-menu-end user-dd animated" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="javascript:void(0)"><i class="mdi mdi-account me-1 ms-1"></i> My Profile</a>
+                                <a class="dropdown-item" href="../users/profile.php"><i class="mdi mdi-account me-1 ms-1"></i> My Profile</a>
                                 <a class="dropdown-item" href="javascript:void(0)"><i class="mdi mdi-wallet me-1 ms-1"></i> My Balance</a>
                                 <a class="dropdown-item" href="javascript:void(0)"><i class="mdi mdi-email me-1 ms-1"></i> Inbox</a>
                                 <div class="dropdown-divider"></div>
                                 <a class="dropdown-item" href="javascript:void(0)"><i class="mdi mdi-settings me-1 ms-1"></i> Account
                                     Setting</a>
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="javascript:void(0)"><i class="fa fa-power-off me-1 ms-1"></i> Logout</a>
+                                <a class="dropdown-item" href="../users/logout.php"><i class="fa fa-power-off me-1 ms-1"></i> Logout</a>
                                 <div class="dropdown-divider"></div>
                                 <div class="ps-4 p-10">
                                     <a href="javascript:void(0)" class="btn btn-sm btn-success btn-rounded text-white">View Profile</a>
@@ -298,7 +313,7 @@
                                     <a href="create.php" class="sidebar-link"><i class="mdi mdi-note-outline"></i><span class="hide-menu"> Create Post </span></a>
                                 </li>
                                 <li class="sidebar-item">
-                                    <a href="form-wizard.html" class="sidebar-link"><i class="mdi mdi-note-plus"></i><span class="hide-menu"> Post List </span></a>
+                                    <a href="../posts/index.php" class="sidebar-link"><i class="mdi mdi-note-plus"></i><span class="hide-menu"> Post List </span></a>
                                 </li>
                             </ul>
                         </li>
--- admin/views/users/profile.php
+++ admin/views/users/profile.php
@@ -5,7 +5,7 @@ use Admin\Post\Post;
 
 session_start();
 
-if (!isset($_SESSION['user']) || empty($_SESSION['user'])) {
+if(!isset($_SESSION['user']) || empty($_SESSION['user'])) {
   header("Location: ../users/login.php");
   exit();
 }
@@ -35,6 +35,7 @@ $posts = $post->index();
   <link href="../../assets/libs/flot/css/float-chart.css" rel="stylesheet" />
   <!-- Custom CSS -->
   <link href="../../dist/css/style.min.css" rel="stylesheet" />
+  <link href="../../dist/css/profile.css" rel="stylesheet" />
   <link rel="stylesheet" type="text/css" href="../../assets/libs/quill/dist/quill.snow.css" />
   <link href="../assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.css" rel="stylesheet" />
   <link rel="stylesheet" type="text/css" href="../assets/extra-libs/multicheck/multicheck.css" />
@@ -317,7 +318,7 @@ $posts = $post->index();
               <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="mdi mdi-receipt"></i><span class="hide-menu">Posts </span></a>
               <ul aria-expanded="false" class="collapse first-level">
                 <li class="sidebar-item">
-                  <a href="create.php" class="sidebar-link"><i class="mdi mdi-note-outline"></i><span class="hide-menu"> Create Post </span></a>
+                  <a href="../posts/create.php" class="sidebar-link"><i class="mdi mdi-note-outline"></i><span class="hide-menu"> Create Post </span></a>
                 </li>
                 <li class="sidebar-item">
                   <a href="../posts/index.php" class="sidebar-link"><i class="mdi mdi-note-plus"></i><span class="hide-menu"> Post List </span></a>
@@ -418,7 +419,7 @@ $posts = $post->index();
       <div class="page-breadcrumb">
         <div class="row">
           <div class="col-12 d-flex no-block align-items-center">
-            <h4 class="page-title">Post List</h4>
+            <h4 class="page-title">My Profile</h4>
             <div class="ms-auto text-end">
               <nav aria-label="breadcrumb">
                 <ol class="breadcrumb">
@@ -439,43 +440,62 @@ $posts = $post->index();
       <!-- Container fluid  -->
       <!-- ============================================================== -->
       <div class="container-fluid">
-        <?php
-        if (isset($_SESSION['message'])) {
-          echo $_SESSION['message'];
-          unset($_SESSION['message']);
-        }
-        ?>
-        <div class="row">
-          <div class="col-md-2"></div>
-          <div class="col-md-8">
-            <div class="table-responsive">
-              <table id="zero_config" class="table table-striped table-bordered">
-                <thead>
-                  <tr>
-                    <th>#SL</th>
-                    <th>Title</th>
-                    <th>Option</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <?php
-                  for ($i = 0; $i < count($posts); $i++) {
-                  ?>
-                    <tr>
-                      <td> <?php echo $i + 1 ?> </td>
-                      <td><?php echo $posts[$i]['title'] ?></td>
-                      <td><a href="../../../frontend/post.php?id=<?php echo $posts[$i]['id'] ?>" target="_blank">Preview</a> |
-                        <a href="edit.php?id=<?php echo $posts[$i]['id'] ?>">Edit</a> |
-                        <a href="delete.php?id=<?php echo $posts[$i]['id'] ?>" onclick="return confirm('Are you sure you want to delete this post.?');">Delete </a>
-                      </td>
-                    </tr>
-                  <?php } ?>
-                </tbody>
-              </table>
+      <?php
+    if (isset($_SESSION['message'])) {
+        echo $_SESSION['message'];
+        unset($_SESSION['message']);
+    }
+    ?>
+        <div class="page-content page-container" id="page-content">
+    <div class="padding">
+        <div class="row container d-flex justify-content-center">
+            <div class="col-xl-6 col-md-12">
+                <div class="card user-card-full">
+                    <div class="row m-l-0 m-r-0">
+                        <div class="col-sm-4 bg-c-lite-green user-profile">
+                            <div class="card-block text-center text-white">
+                                <div class="m-b-25"> <img src="https://img.icons8.com/bubbles/100/000000/user.png" class="img-radius" alt="User-Profile-Image"> </div>
+                                <h6 class="f-w-600">Shohanur Rahman</h6>
+                                <p>Web Designer</p> <i class=" mdi mdi-square-edit-outline feather icon-edit m-t-10 f-16"></i>
+                            </div>
+                        </div>
+                        <div class="col-sm-8">
+                            <div class="card-block">
+                                <h6 class="m-b-20 p-b-5 b-b-default f-w-600">Information</h6>
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <p class="m-b-10 f-w-600">Email</p>
+                                        <h6 class="text-muted f-w-400"><?php echo $_SESSION['user']['email'] ?></h6>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <p class="m-b-10 f-w-600">Username</p>
+                                        <h6 class="text-muted f-w-400"><?php echo $_SESSION['user']['username'] ?></h6>
+                                    </div>
+                                </div>
+                                <h6 class="m-b-20 m-t-40 p-b-5 b-b-default f-w-600">Projects</h6>
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <p class="m-b-10 f-w-600">Recent</p>
+                                        <h6 class="text-muted f-w-400">Sam Disuja</h6>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <p class="m-b-10 f-w-600">Most Viewed</p>
+                                        <h6 class="text-muted f-w-400">Dinoter husainm</h6>
+                                    </div>
+                                </div>
+                                <ul class="social-link list-unstyled m-t-40 m-b-10">
+                                    <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="facebook" data-abc="true"><i class="mdi mdi-facebook feather icon-facebook facebook" aria-hidden="true"></i></a></li>
+                                    <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="twitter" data-abc="true"><i class="mdi mdi-twitter feather icon-twitter twitter" aria-hidden="true"></i></a></li>
+                                    <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="instagram" data-abc="true"><i class="mdi mdi-instagram feather icon-instagram instagram" aria-hidden="true"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div class="col-md-2"></div>
         </div>
+    </div>
+</div>
       </div>
       <!-- ============================================================== -->
       <!-- End Container fluid  -->
